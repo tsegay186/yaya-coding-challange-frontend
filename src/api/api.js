5e8fb3c62d1fb008a25acea89716fe2c9d922414
tsegay <tsegay.berhanu186@gmail.com>
2024-02-17 20:55:03 -0800

    updating url

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
 export const getTransactions = async (p) => {
   const url = p
-    ? `http://localhost:3000/transactions?p=${p}`
-    : `http://localhost:3000/transactions`;
+    ? `https://yaya-coding-challenge.onrender.com/transactions?p=${p}`
+    : `https://yaya-coding-challenge.onrender.com/transactions`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
 };
 
 export const searchTransactions = async (searchTerm, p) => {
-  const url = `http://localhost:3000/transactions/search?query=${searchTerm}&p=${p}`
+  const url = `https://yaya-coding-challenge.onrender.com/transactions/search?query=${searchTerm}&p=${p}`
   const response = await fetch(url)
   const result = await response.json()
   return result 
